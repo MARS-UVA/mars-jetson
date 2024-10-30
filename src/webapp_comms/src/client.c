7e94db03c1fdeca819bb081ff3fb38fea496f3c1
mars_host <mars_host@mars-lunabotics.myguest.virtualbox.org>
2024-10-30 17:01:36 -0400

    made it connect to a specfic ip address

--- src/webapp_comms/src/client.c
+++ src/webapp_comms/src/client.c
@@ -14,14 +14,14 @@
 
 #define PORT 8080
 
-void client()
+void client(char ip)
 {
     int client_fd, client_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char ack[4] = "";
+    char ack[1000];
 
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
@@ -30,20 +30,27 @@ void client()
     }
 
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = inet_addr(ip);
     address.sin_port = htons(PORT);
 
-    if (connect(client_fd, address, sizeOf(address)) == 0) 
+    if (connect(client_fd, (struct sockaddr*)&address, sizeOf(address)) == 0) 
     {
         perror("Failure to connect");
         exit(EXIT_FAILURE);
     }
-
+    while(1){
     send(client_fd, buffer, 1024, 0);
-    valread = read(client_fd, ack, 4);
+    valread = read(client_fd, ack, 1000);
+    if(valread == 0){
+        break;
+    }
+    ack[valread] = "\0";
     printf("\n%s", ack);
-
+    }
     close(client_socket);
-
     return 0;
+}
+
+int main(int argc , char argv[]){
+    client(argv[1]);
 }
