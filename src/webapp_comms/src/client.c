b9e0c2d78894937ec75830977f1db2da54b7cc5f
Eric Paschke <edpaschke@gmail.com>
2024-11-03 14:49:31 -0500

    removed while loop in client socket

--- src/webapp_comms/src/client.c
+++ src/webapp_comms/src/client.c
@@ -38,24 +38,20 @@ void client(char* ip)
         exit(EXIT_FAILURE);
     }
      printf("Connected to server at %s:%d\n", ip, PORT);
-    while(1){
+
     strncpy(buffer, "hello", 5);
     if (send(client_fd, buffer, strlen(buffer), 0) == -1) {
             perror("Send failed");
-            break;
     }
 
     valread = read(client_fd, ack, 1000);
     if (valread < 0) {
             perror("Read failed");
-            break;
         } else if (valread == 0) {
             printf("Server closed the connection\n");
-            break;
         }
     ack[valread] = '\0';
     printf("%s\n", ack);
-    }
     close(client_fd);
     return;
 }
