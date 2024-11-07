2a8bc6d5c3385dbfab6bbee26400606a4fb9d61c
Eric Paschke <edpaschke@gmail.com>
2024-11-07 18:01:37 -0500

    added dummy img_pub and img_sub files

---
+++ src/webapp_comms/src/image_sub.cpp
@@ -0,0 +1,42 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+
+class WebcamSubscriber : public rclcpp::Node {
+public:
+    WebcamSubscriber() : Node("webcam_subscriber") {
+        RCLCPP_INFO(this->get_logger(), "Webcam subscriber node initialized");
+
+        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "webcam_topic", 
+            10, 
+            std::bind(&WebcamSubscriber::listener_callback, this, std::placeholders::_1)
+        );
+    }
+
+private:
+    void listener_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+        } catch (cv_bridge::Exception& e) {
+            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        cv::imshow("Image", cv_ptr->image);
+        cv::waitKey(1);
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+};
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<WebcamSubscriber>();
+    rclcpp::spin(node);
+    cv::destroyAllWindows();
+    rclcpp::shutdown();
+    return 0;
+}
