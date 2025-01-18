fae7fbe2986ec3ec080dbfc15735dbb0bb30d645
mars_host <mars_host@mars-lunabotics.myguest.virtualbox.org>
2025-01-18 14:40:24 -0500

    attempted to retrieve camera details

--- src/webcam/scripts/webcam_publisher.py
+++ src/webcam/scripts/webcam_publisher.py
@@ -14,6 +14,7 @@ webcam_image (type sensor_msgs.msg.Image): BGR8 images from a webcam (QOS: 10).
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+#from sensor_msgs.msg import CameraInfo
 
 # OpenCV imports, use 'pip install opencv-python' to get OpenCV for Python
 from cv_bridge import CvBridge
@@ -32,13 +33,18 @@ class WebcamPublisher(Node):
     def __init__(self):
         super().__init__('webcam')  # Calling base class to assign node name 
         self.publisher_ = self.create_publisher(Image, 'webcam_image', 10)
+        #self.publisher_ = self.create_publisher(CameraInfo,'webcam_info', 10)
         # The second parameter is there because we were having issues with
         # GStreamer. CAP_V4L2 refers to Video for Linux 2, which we're using
         # as an alternative.
         self.cap = cv2.VideoCapture(VIDEO_CAPTURE_PORT, cv2.CAP_V4L2)
+        self.info_width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.info_height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.bridge = CvBridge()
         self.timer = self.create_timer(1 / FRAME_RATE_PER_SECOND,
                                        self.publish_frame)
+        # self.timer2 = self.create_timer(1 / FRAME_RATE_PER_SECOND,
+        #                                self.publish_camera_info)
 
     def publish_frame(self):
         """
@@ -49,7 +55,16 @@ class WebcamPublisher(Node):
         if ret:
             img_msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
             self.publisher_.publish(img_msg)
+            print("height ", self.info_height)
+            print("width ", self.info_width)
 
+    # def publish_camera_info(self):
+    #     cam_msg = self.info_width
+    #     self.publisher_.publish(cam_msg)
+    #     self.get_logger().info('Publishing: "%d"' % cam_msg)
+    #     cam_msg = self.info_height
+    #     self.publisher_.publish(cam_msg)
+    #     self.get_logger().info('Publishing: "%d"' % cam_msg)
     def destroy_node(self):
         super().destroy_node()  # Release the video capture on node destruction
         self.cap.release()
