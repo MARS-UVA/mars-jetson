c0cf6f9e460f7ab94a91a880c3c52344f8c4f715
Eric Paschke <edpaschke@gmail.com>
2025-02-23 14:50:50 -0500

    added sub to net_node

--- src/webapp_comms/src/net_node.cpp
+++ src/webapp_comms/src/net_node.cpp
@@ -4,23 +4,32 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+
+#include "./client.hpp"
+#include "./server.hpp"
 
 using namespace std::chrono_literals;
 
 
-class netNode : public rclcpp::Node
+class NetNode : public rclcpp::Node
 {
   public:
-    netNode()
-    : Node("net_node"), count_(0)
+    NetNode()
+    : Node("NetNode"), count_(0)
     {
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      subscription_ = this->create_subscriber<std_msgs:msg::String>(
+      "topic", 10, std::bind(&netNode::topic_callback, this, _1));
     }
 
   private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+
     void timer_callback()
     {
       auto message = std_msgs::msg::String();
@@ -28,15 +37,17 @@ class netNode : public rclcpp::Node
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
+    
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<NetNode>());
   rclcpp::shutdown();
   return 0;
 }
