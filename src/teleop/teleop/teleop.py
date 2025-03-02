94030c917ee49ff67ad0d1671f608354a5e32149
Miles Brickhouse <brickhouse.m2309@gmail.com>
2025-03-02 19:07:17 +0000

    changed to incorporate HumanInputState message

--- src/teleop/teleop/teleop.py
+++ src/teleop/teleop/teleop.py
@@ -3,7 +3,7 @@ import sys
 
 import rclpy
 from rclpy.node import Node
-from teleop_msgs.msg import GamepadState
+from teleop_msgs.msg import HumanInputState
 
 from .control import DriveControlStrategy, ArcadeDrive
 
@@ -13,10 +13,10 @@ class TeleopNode(Node):
     def __init__(self, drive_control_strategy: DriveControlStrategy, **kwargs):
         super().__init__('teleop', **kwargs)
         self.__drive_control_strategy = copy.copy(drive_control_strategy)
-        self.__gamepad_state_subscription = self.create_subscription(
-            msg_type=GamepadState,
-            topic='gamepad_state',
-            callback=self.__on_receive_gamepad_state,
+        self.__human_input_state_subscription = self.create_subscription(
+            msg_type=HumanInputState,
+            topic='human_input_state',
+            callback=self.__on_receive_human_input_state,
             qos_profile=10,
         )
 
@@ -31,12 +31,12 @@ class TeleopNode(Node):
 
         self.__drive_control_strategy = copy.copy(value)
 
-    def __on_receive_gamepad_state(self, gamepad_state: GamepadState) -> None:
-        wheel_speeds = self.__drive_control_strategy.get_wheel_speeds(gamepad_state)
+    def __on_receive_human_input_state(self, human_input_state: HumanInputState) -> None:
+        wheel_speeds = self.__drive_control_strategy.get_wheel_speeds(human_input_state)
 
         self.get_logger().info(f'Calculated: {wheel_speeds}')
 
-        # TODO: Finish implementing TeleopNode.__on_receive_gamepad_state
+        # TODO: Finish implementing TeleopNode.__on_receive_human_input_state
 
 
 def main() -> None:
