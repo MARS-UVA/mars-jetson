7abd39dc0aabb3635aac588fa38d969fdc36c92a
Ivan Post <ivan.post24@gmail.com>
2025-03-23 19:42:41 +0000

    All tests pass

--- src/teleop/teleop/teleop.py
+++ src/teleop/teleop/teleop.py
@@ -144,11 +144,6 @@ class TeleopNode(Node):
 
         wheel_speed_msg = wheel_speed_to_motor_queries(wheel_speeds)
         wheel_speed_msg.changes.append(bucket_actuator_speed(human_input_state))
-        #Hopefully this will only send actuator message when Right Trigger is pressed
-        # right_trigger=human_input_state.gamepad_state.rt_pressed
-        # if right_trigger > 0.1:
-        #     right_trigger_velocity = round(right_trigger*255)
-        #     wheel_speed_msg.changes.append(SetMotor(index=SetMotor.BUCKET_DRUM_ACTUATOR, velocity = right_trigger_velocity))
         self._wheel_speed_publisher.publish(wheel_speed_msg)
 
     def __add_parameter_event_handlers(self) -> None:
