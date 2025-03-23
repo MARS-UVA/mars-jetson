a48f721a58cb117b64a703aa33de7defb0c554cc
Ivan Post <ivan.post24@gmail.com>
2025-03-23 19:33:53 +0000

    Finished test cases

--- src/teleop/test/test_bucket_drum_input.py
+++ src/teleop/test/test_bucket_drum_input.py
@@ -1,4 +1,4 @@
-
+from teleop.motor_queries import bucket_actuator_speed
 from teleop_msgs.msg import HumanInputState, SetMotor
 
 
@@ -6,8 +6,8 @@ def test_dpad_up_causes_actuator_up() -> None:
     state = HumanInputState()
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
     state.gamepad_state.du_pressed = True
-    motor_changes = ...
-    actuator_value = next((command.value for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
+    motor_changes = bucket_actuator_speed(state)
+    actuator_value = next((command.velocity for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
     assert actuator_value is not None, 'Actuator value was not changed'
     assert actuator_value > 127
 
@@ -16,8 +16,8 @@ def test_dpad_down_causes_actuator_down() -> None:
     state = HumanInputState()
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
     state.gamepad_state.dd_pressed = True
-    motor_changes = ...
-    actuator_value = next((command.value for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
+    motor_changes = bucket_actuator_speed(state)
+    actuator_value = next((command.velocity for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
     assert actuator_value is not None, 'Actuator value was not changed'
     assert actuator_value < 127
 
@@ -25,8 +25,8 @@ def test_dpad_down_causes_actuator_down() -> None:
 def test_no_dpad_causes_actuator_zero() -> None:
     state = HumanInputState()
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
-    motor_changes = ...
-    actuator_value = next((command.value for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
+    motor_changes = bucket_actuator_speed(state)
+    actuator_value = next((command.velocity for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
     assert actuator_value is not None, 'Actuator value was not changed'
     assert actuator_value == 127
 
@@ -36,7 +36,7 @@ def test_both_dpad_causes_actuator_zero() -> None:
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
     state.gamepad_state.du_pressed = True
     state.gamepad_state.dd_pressed = True
-    motor_changes = ...
-    actuator_value = next((command.value for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
+    motor_changes = bucket_actuator_speed(state)
+    actuator_value = next((command.velocity for command in motor_changes.changes if command.index == SetMotor.BUCKET_DRUM_ACTUATOR), None)
     assert actuator_value is not None, 'Actuator value was not changed'
     assert actuator_value == 127
