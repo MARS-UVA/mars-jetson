e18effee74902a3ca48a943199004c4ed27afcee
Miles Brickhouse <brickhouse.m2309@gmail.com>
2025-04-06 20:19:34 +0000

    maybe tests work

--- src/teleop/test/test_bucket_drum_input.py
+++ src/teleop/test/test_bucket_drum_input.py
@@ -36,23 +36,23 @@ def test_both_dpad_causes_actuator_zero() -> None:
 def test_cruise_control_rt_causes_drum_speed_increase() -> None:
     state = HumanInputState
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
-    state.gamepad_state.lt_pressed=1
-    state.gamepad_state.rt_pressed=0
+    state.gamepad_state.lt_pressed = 1
+    state.gamepad_state.rt_pressed = 0
     bucket_drum_set = bucket_drum_speed_cruise_control(state, 127)
     assert bucket_drum_set > 127
 
 def test_cruise_control_lt_causes_drum_speed_decrease() -> None:
     state = HumanInputState
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
-    state.gamepad_state.lt_pressed=0
-    state.gamepad_state.rt_pressed=1
+    state.gamepad_state.lt_pressed = 0
+    state.gamepad_state.rt_pressed = 1
     bucket_drum_set = bucket_drum_speed_cruise_control(state, 127)
     assert bucket_drum_set < 127
 
 def test_cruise_control_lt_and_rt_causes_nothing() -> None:
     state = HumanInputState
     state.drive_mode = HumanInputState.DRIVEMODE_TELEOP
-    state.gamepad_state.lt_pressed=1
-    state.gamepad_state.rt_pressed=1
+    state.gamepad_state.lt_pressed = 1
+    state.gamepad_state.rt_pressed = 1
     bucket_drum_set = bucket_drum_speed_cruise_control(state, 127)
     assert bucket_drum_set == 127
