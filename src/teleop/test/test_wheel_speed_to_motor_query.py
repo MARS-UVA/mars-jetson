0f04bb410afcb24a6da3cae70320781e33593f2f
Miles Brickhouse <brickhouse.m2309@gmail.com>
2025-04-06 18:00:39 +0000

    added test for stop_motors function

--- src/teleop/test/test_wheel_speed_to_motor_query.py
+++ src/teleop/test/test_wheel_speed_to_motor_query.py
@@ -1,5 +1,5 @@
 from teleop.control import WheelSpeeds
-from teleop.motor_queries import wheel_speed_to_motor_queries
+from teleop.motor_queries import wheel_speed_to_motor_queries, stop_motors
 from teleop_msgs.msg import MotorChanges, SetMotor
 def check_speeds(observed: MotorChanges, expected: MotorChanges) -> None:
     indices_in_observed={}
@@ -64,3 +64,17 @@ def test_negative_wheel_speed_two() -> None:
                                  SetMotor(index=SetMotor.BACK_RIGHT_DRIVE_MOTOR, velocity=0)])
     #maintain dict with expected values for indices and maintain set of checked indicies; loop through and if index is in set, index dictionary and remove index from set: check if index is in dictionary
     check_speeds(observed_speed, expected_speed)
+def test_stop_motors() -> None:
+    speed = WheelSpeeds(1.0,1.0)
+    observed_speed= wheel_speed_to_motor_queries(speed)
+    observed_speed.changes.append(SetMotor(index=SetMotor.BUCKET_DRUM_SPIN_MOTOR, velocity = 255))
+    observed_speed.changes.append(SetMotor(index=SetMotor.BUCKET_DRUM_ACTUATOR, velocity = 191))
+    expected_speed = MotorChanges(changes=[SetMotor(index=SetMotor.FRONT_LEFT_DRIVE_MOTOR, velocity=127),
+                                 SetMotor(index=SetMotor.BACK_LEFT_DRIVE_MOTOR, velocity=127),
+                                 SetMotor(index=SetMotor.FRONT_RIGHT_DRIVE_MOTOR, velocity=127),
+                                 SetMotor(index=SetMotor.BACK_RIGHT_DRIVE_MOTOR, velocity=127),
+                                 SetMotor(index=SetMotor.BUCKET_DRUM_SPIN_MOTOR, velocity=127),
+                                 SetMotor(index=SetMotor.BUCKET_DRUM_ACTUATRO, velocity = 127)])
+    assert not check_speeds(observed_speed,expected_speed)
+    observed_speed=stop_motors()
+    check_speeds (observed_speed, expected_speed)
