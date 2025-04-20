a09220ffd9cc56f7062e4de8afa423602de650a7
root <ericzn248@gmail.com>
2025-04-19 21:39:14 -0400

    added new files from broken serial-comms branch

--- src/serial_ros/serial_ros/node.py
+++ src/serial_ros/serial_ros/node.py
@@ -1,25 +1,34 @@
 import rclpy
 from rclpy.node import Node
 from serial_ros.serial_handler import SerialHandler
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
 from teleop_msgs.msg import MotorChanges
+from nucleo_msgs.msg import Feedback
 
 MOTOR_CURRENT_MSG = 0
 SEND_DELAY_SEC = 0.1
+RECV_DELAY_SEC = 0.5
 MOTOR_STILL = 127
 
 class SerialNode(Node):
 
     def __init__(self):
-        self.data = [MOTOR_STILL]*6 # 0:header, [0:4]:4 wheels, 4:bucket drum, 5:linear actuator
+        self.motorData = [MOTOR_STILL]*6 # 0:header, [0:4]:4 wheels, 4:bucket drum, 5:linear actuator
         super().__init__('read_from_teleop')
         self.subscription = self.create_subscription(
-            MotorChanges,
-            'teleop',
-            self.listener_callback,
-            1) #1 queued message
+            msg_type=MotorChanges,
+            topic='teleop',
+            callback=self.updateCurrents,
+            qos_profile=1 #1 queued message
+        ) 
+        self.feedback_publisher = self.create_publisher(
+            msg_type=Feedback,
+            topic='feedback',
+            qos_profile=1
+        )
         self.subscription  # prevent unused variable warning
-        self.timed_publisher = self.create_timer(SEND_DELAY_SEC, self.sendCurrents)
+        self.send_timer = self.create_timer(SEND_DELAY_SEC, self.sendCurrents)
+        self.recv_timer = self.create_timer(RECV_DELAY_SEC, self.readFromNucleo)
         self.serial_handler = SerialHandler()
 
     def listener_callback(self, msg):
@@ -29,18 +38,30 @@ class SerialNode(Node):
             self.get_logger().info(f"{motors[i]}: {change.velocity}")
             # self.get_logger().info("Hey")
             # self.get_logger().info(type(change.velocity))
-            self.data[change.index] = change.velocity
+            self.motorData[change.index] = change.velocity
             i+=1
 
 
-        # for field in self.data:
+        # for field in self.motorData:
         #     self.get_logger().info(str(field))
         
     def sendCurrents(self):
         #print(ok)
         # self.get_logger().info("Sending currents")
-        self.serial_handler.send(MOTOR_CURRENT_MSG, self.data)
-        
+        self.serial_handler.send(MOTOR_CURRENT_MSG, self.motorData)
+    
+    def readFromNucleo(self): 
+        data = self.serial_handler.readMsg()
+        mf = Feedback(front_left = data[0],
+                            front_right = data[1],
+                            back_left = data[2],
+                            back_right = data[3],
+                            drum = data[4],
+                            l_actuator = data[5],
+                            r_actuator = data[6],
+                            actuator_height = data[7])
+        self.feedback_publisher.publish(mf)
+            
 
 def main(args=None):
     rclpy.init(args=args)
@@ -48,10 +69,6 @@ def main(args=None):
     node = SerialNode()
 
     rclpy.spin(node)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     node.destroy_node()
     rclpy.shutdown()
 
