e260b848716e63f969acb2fd118656c82bbe6135
Mikhail <mkornilov07@gmail.com>
2025-04-24 22:39:56 -0400

    also cook nucleo messages package

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/mars-jetson/src
-ROS_PACKAGES=(webapp_comms teleop teleop_msgs startup serial_ros)
+ROS_PACKAGES=(webapp_comms teleop teleop_msgs startup serial_ros nucleo_msgs)
 for package in ${ROS_PACKAGES[@]}; do
     rm -r "$package/build"
     rm -r "$package/install"
