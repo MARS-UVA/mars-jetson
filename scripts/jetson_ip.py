f62ca952c1c62327e7abf28464bbf566fec39018
ericzn248 <ericzn248@gmail.com>
2025-11-16 14:13:25 -0500

    add jetson ip script

---
+++ scripts/jetson_ip.py
@@ -0,0 +1,24 @@
+import requests
+import socket
+
+# Replace with your webhook URL
+WEBHOOK_URL = "https://discord.com/api/webhooks/1435393485262946445/kpzKJ9iZ4o8wSZdboCGKRg_2LlTLTAHPFNhh_sUDncrlvFW1xgsy4BsUjbuznVZzhlUu"
+
+# Get local IP (Jetson's eduroam IP)
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Doesn't need to connect, just to get the IP used to reach outside
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except:
+        ip = "Unable to get IP"
+    finally:
+        s.close()
+    return ip
+
+ip_address = get_ip()
+
+# Post to Discord
+data = {"content": f"Jetson IP: {ip_address}"}
+requests.post(WEBHOOK_URL, json=data)
