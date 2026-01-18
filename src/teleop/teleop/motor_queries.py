97413e217b4d81a848d3c0be139667099b415321
Mikhail Kornilov <mikhail@virginia.edu>
2026-01-18 12:12:38 -0500

    drum and arm interfaces for double drum teleop

--- src/teleop/teleop/motor_queries.py
+++ src/teleop/teleop/motor_queries.py
@@ -1,4 +1,4 @@
-from teleop_msgs.msg import MotorChanges, SetMotor, HumanInputState
+from teleop_msgs.msg import MotorChanges, SetMotor, AddMotor, HumanInputState
 from .control import WheelSpeeds
 #function converting wheel speeds to motor queries
 def wheel_speed_to_motor_queries(wheel_speeds: WheelSpeeds) -> MotorChanges:
@@ -10,17 +10,27 @@ def wheel_speed_to_motor_queries(wheel_speeds: WheelSpeeds) -> MotorChanges:
                                  SetMotor(index=SetMotor.BACK_RIGHT_DRIVE_MOTOR, velocity=right_wheel_speeds)],
                         adds = [])
 
-def bucket_actuator_speed(human_input: HumanInputState) -> SetMotor:
-    actuator_value = 127
-    ACTUATOR_BIG_SPEED = 127
-    ACTUATOR_SMALL_SPEED_MAX = 64
-    if human_input.gamepad_state.du_pressed and not human_input.gamepad_state.dd_pressed:
-        actuator_value = 127 + ACTUATOR_BIG_SPEED
-    elif human_input.gamepad_state.dd_pressed and not human_input.gamepad_state.du_pressed:
-        actuator_value = 127 - ACTUATOR_BIG_SPEED
-    else:
-        actuator_value = 127 + int(human_input.gamepad_state.right_stick.y * ACTUATOR_SMALL_SPEED_MAX)
-    return SetMotor(index=SetMotor.BUCKET_DRUM_ACTUATOR, velocity = actuator_value)
+def increment_drum_spin(increment : int, left_spin: bool, right_spin: bool, start_msg: MotorChanges):
+    if left_spin:
+        start_msg.adds.append(AddMotor(index = SetMotor.SPIN_LEFT_MOTOR, vel_increment = increment))
+    if right_spin:
+        start_msg.adds.append(AddMotor(index = SetMotor.SPIN_RIGHT_MOTOR, vel_increment = increment))
+
+    
+
+def raise_arms(actuator_vel_signed, left_control : bool, right_control : bool, start_msg: MotorChanges) -> SetMotor:
+    actuator_vel = actuator_vel_signed + 127
+    if left_control:
+        start_msg.changes.append(SetMotor(index = SetMotor.ARM_LEFT_ACTUATOR, velocity = actuator_vel))
+    if right_control:
+        start_msg.changes.append(SetMotor(index = SetMotor.ARM_RIGHT_ACTUATOR, velocity = actuator_vel))
+    
+
+    
+def stop_spin(start_msg: MotorChanges) -> None:
+    start_msg.changes += [SetMotor(index = SetMotor.SPIN_LEFT_MOTOR, velocity = 127),
+                                   SetMotor(index = SetMotor.SPIN_RIGHT_MOTOR, velocity = 127)]
+
 def stop_motors() -> MotorChanges:
     return MotorChanges(changes=[SetMotor(index=SetMotor.FRONT_LEFT_DRIVE_MOTOR, velocity=127),
                                  SetMotor(index=SetMotor.BACK_LEFT_DRIVE_MOTOR, velocity=127),
