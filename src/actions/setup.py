d873f672ca1e5d910e84965baa643bb510704712
Eric Paschke <edpaschke@gmail.com>
2026-01-18 15:45:38 -0500

    added teleop and teleop_msgs as dependencies

--- src/actions/setup.py
+++ src/actions/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'teleop', 'teleop_msgs'],
     zip_safe=True,
     maintainer='xerix',
     maintainer_email='edpaschke@gmail.com',
