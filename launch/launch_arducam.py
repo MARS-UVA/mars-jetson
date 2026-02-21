2caf27772b25eb57c5ceff1e82c26962f2837bc5
marsatuva <marsatuva@gmail.com>
2026-02-20 19:38:28 -0500

    New camera and AprilTag data

--- launch/launch_arducam.py
+++ launch/launch_arducam.py
@@ -12,7 +12,7 @@ def generate_launch_description():
                 'video_device': '/dev/v4l/by-id/usb-Arducam_Technology_Co.__Ltd._Arducam_OV9281_USB_Camera_UC762-video-index0',
                 'image_size': [1280, 720],
                 'output_encoding': 'yuv422_yuy2',
-                # 'camera_info_url': 'file:///home/ericzn248/.ros/camera_info/narrow_stereo.yaml',
+                'camera_info_url': 'file:///home/mars/mars-jetson/calibrations/arducam1.yaml',
             }]
         ),
     ])
