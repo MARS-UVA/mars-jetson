1c157a2a10ed8d9abeed5381216400d55e650ec7
Enoch Tan <enocht@KhimeraET.localdomain>
2026-04-22 20:44:43 -0400

    created robot_descriptor parameter and dummy link to the urdf

--- src/startup/launch/transforms.launch.py
+++ src/startup/launch/transforms.launch.py
@@ -5,6 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command
 from launch_ros.actions import Node
 
 
@@ -25,6 +26,9 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
-            arguments=[urdf]),
+            parameters=[
+                {'use_sim_time': use_sim_time},
+                {'robot_description': Command(['cat ', urdf])}
+            ]
+        )
     ])
