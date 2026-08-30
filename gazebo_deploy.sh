a6ee63d10b367727328628817ac0f9c787a83b80
Ethan Edwards <ethanvolcanic@gmail.com>
2026-08-30 16:45:15 +0000

    updated gazebo.launch.py to run the important packages and made gazebo deploy script (untested)

---
+++ gazebo_deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Usage:
+#Option 1: ./deploy.sh
+#Option 2: ./deploy.sh <control station ip>
+
+cd ~/mars-jetson
+
+
+if [ $# -ge 1 ];
+    then
+        export CONTROL_STATION_IP=$1
+    else 
+        export CONTROL_STATION_IP="192.168.50.60"
+fi
+
+# Build the packages
+# colcon build --packages-select teleop_msgs serial_msgs
+colcon build --symlink-install --packages-ignore zed_components zed_wrapper zed_ros2 --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=true
+# Source the setup file
+source install/setup.bash
+
+# export FASTDDS_BUILTIN_TRANSPORTS=LARGE_DATA
+# export FASTDDS_BUILTIN_TRANSPORTS=UDPv4
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+
+export JETSON_MODEL_NAME=JETSON_ORIN_NANO
+sudo busybox devmem 0x02448030 w 0x40a
+sudo busybox devmem 0x02430098 w 0x05
+
+# Launch everything
+# run realsense executable in parallel
+#ros2 launch startup launch.py
+# ~/mars-jetson/src/obstacle_detection/src/build/obstacle_detect_node &
+# OBSTACLE_DETECT_PID=$!
+
+ros2 launch lunabotics_simulation gazebo.launch.py
+
+# kill $OBSTACLE_DETECT_PID
--- src/lunabotics_simulation/launch/gazebo.launch.py
+++ src/lunabotics_simulation/launch/gazebo.launch.py
@@ -55,6 +55,7 @@ def generate_launch_description():
         return [node_robot_state_publisher]
 
 
+    # Nodes important for running the simulation and controlling the robot
     gz_spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
@@ -97,6 +98,101 @@ def generate_launch_description():
         arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
         output='screen'
     )
+    
+    # Nodes important for connecting the robot to the Control Station and running robot movements
+    teleop = Node(
+                    package='teleop',
+                    executable='teleop',
+                    name='teleop',
+                    output='screen',
+                    parameters=[{
+                        'linear_axis': 'left_y',
+                        'turn_axis': 'left_x_inverted',
+                        'full_forward_magnitude': 0.6,
+                        'deadband': 0.05
+                    }],
+                    arguments=['--ros-args', '--log-level', 'WARN'],
+                    respawn=True
+                )
+    
+    digdump = Node(
+                package='digdump',
+                executable='action_server',
+                name='digdump',
+                output='screen',
+                parameters=[{
+                    'actuator_speed_aerial': 1.0,
+                    'actuator_speed_ground': 0.75,
+                    'dig_speed': 0.75,
+                    'dump_speed': 1.0,
+                    'drive_speed': 0.25,
+                    'dig_time': 3.0,
+                    'dump_time': 6.0,
+                    'move_time': 6.0,
+                    'actuator_extend_length_aerial': 0.69,
+                    'actuator_extend_length_ground': 0.9,
+                }],
+                respawn=True
+            )
+    network_client = Node(
+                package='network_communication',
+                executable='udp_client',
+                name='client_node',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'WARN'],
+                respawn=True
+            )
+    network_server = Node(
+                package='network_communication',
+                executable='udp_server',
+                name='server_node',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'WARN'],
+                respawn=True
+            )
+    serial = Node(
+                package='serial_node',
+                executable='op_reader',
+                name='serial_node',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'WARN'],
+                parameters=[
+                    {'mock_serial':'1'}
+                ],
+                respawn=True
+            )
+    controller = Node(
+                package='robot_controller',
+                executable='robot_controller',
+                name='robot_controller',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'WARN'],
+                respawn=True
+            )
+    cmd_vel_mux = Node(
+                package='topic_tools',
+                executable='mux',
+                name='cmd_vel_mux',
+                output='screen',
+                arguments=['/cmd_vel', '/cmd_vel/teleop', '/cmd_vel/autonomy'],
+                respawn=True
+            )
+    arm_drum_mux = Node(
+                package='topic_tools',
+                executable='mux',
+                name='arm_drum_mux',
+                output='screen',
+                arguments=['/arm_drum_control', '/arm_drum_control/teleop', '/arm_drum_control/autonomy'],
+                respawn=True
+            )
+    robot_state_controller = Node(
+                package='robot_state_controller',
+                executable='robot_state_controller',
+                name='robot_state_controller',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'WARN'],
+                respawn=True
+            )
 
     ld = LaunchDescription([
         SetEnvironmentVariable(
@@ -135,6 +231,15 @@ def generate_launch_description():
             'description_format',
             default_value='urdf',
             description='Robot description format to use, urdf or sdf'),
+        teleop,
+        digdump,
+        network_client,
+        network_server,
+        serial,
+        controller,
+        cmd_vel_mux,
+        arm_drum_mux,
+        robot_state_controller
     ])
     ld.add_action(OpaqueFunction(function=robot_state_publisher))
     return ld
